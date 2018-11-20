0449e36b813972c3f7e92ffee96e3685a0e00159
Felipe Vegners <felipevegners@gmail.com>
2018-11-20 17:03:52 -0200

    Structure Updates

--- hype-tur/header.php
+++ hype-tur/header.php
@@ -58,7 +58,7 @@
         <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
         <p><?php bloginfo('description'); ?></p>
         -->
-    <nav>
-      <!-- <?php wp_nav_menu(); ?> -->
-    </nav>
+    <!-- <nav>
+      <?php wp_nav_menu(); ?>
+    </nav> -->
   </header>
