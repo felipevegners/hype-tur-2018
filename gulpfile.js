1a593b5efc6459c79d0ab77cd6001f35ef99f2e9
Felipe Vegners <felipevegners@gmail.com>
2019-06-25 01:22:52 -0300

    update gulpfile to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,215 +1,84 @@
-//initialize all of our variables
-var app, base, concat, directory, gulp, gutil, hostname, path, refresh, sass, uglify, imagemin, minifyCSS, del, browserSync, autoprefixer, gulpSequence, shell, sourceMaps, plumber;
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const {
+  src,
+  dest,
+  watch,
+  series,
+  parallel
+} = require('gulp')
 
-var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
+// Importing all the Gulp-related packages we want to use
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const plumber = require('gulp-plumber')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+var replace = require('gulp-replace')
+const browserSync = require('browser-sync').create()
 
-//load all of our dependencies
-//add more here if you want to include more libraries
-gulp        = require('gulp');
-gutil       = require('gulp-util');
-concat      = require('gulp-concat');
-uglify      = require('gulp-uglify');
-sass        = require('gulp-sass');
-sourceMaps  = require('gulp-sourcemaps');
-imagemin    = require('gulp-imagemin');
-minifyCSS   = require('gulp-minify-css');
-browserSync = require('browser-sync').create();
-autoprefixer = require('gulp-autoprefixer');
-gulpSequence = require('gulp-sequence').use(gulp);
-shell       = require('gulp-shell');
-plumber     = require('gulp-plumber');
+// File paths
+const files = {
+  scssPath: 'app/styles/scss/init.scss',
+  jsPath: 'app/scripts/src/**/*.js'
+}
 
-gulp.task('browserSync', function() {
+// Sass task: compiles the style.scss file into style.css
 
-  var files = ['wp-content/themes/hypetur/*.php', 'wp-content/themes/hypetur/*.css'];
-    browserSync.init(files, {
-        // server: {
-        //     baseDir: "app/"
-        // },
-        options: {
-            reloadDelay: 250
-        },
-        notify: false,
+function css() {
+  return src(files.scssPath)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(rename('style.css'))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest('wp-content/themes/hypetur'))
+    .pipe(browserSync.stream())
+}
 
-        proxy: "http://localhost:8080"
-    });
-});
+// JS task: concatenates and uglifies JS files to script.js
+function js() {
+  return src([
+      files.jsPath
+      //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+    ])
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(dest('wp-content/themes/hypetur/scripts'))
+}
 
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+// function watchTask() {
+//   watch([files.scssPath, files.jsPath],
+//     parallel(scssTask, jsTask))
+// }
 
-//compressing images & handle SVG files
-gulp.task('images', function(tmp) {
-    gulp.src(['app/images/*.jpg', 'app/images/*.png'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('app/images'));
-});
+function watchTask() {
+  // var files = ['wp-content/themes/hypetur/*.php', 'wp-content/themes/hypetur/*.css'];
+  browserSync.init({
+    // server: {
+    //   baseDir: 'wp-content/themes/hypetur',
+    // },
+    options: {
+      reloadDelay: 250
+    },
+    notify: true,
 
-//compressing images & handle SVG files
-gulp.task('images-deploy', function() {
-    gulp.src(['app/images/**/*', '!app/images/README'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('wp-content/themes/hypetur/images'));
+    proxy: "http://localhost:8080"
+  })
+  watch('wp-content/themes/hypetur/*.css', css)
+  watch('wp-content/themes/hypetur/*.php').on('change', browserSync.reload)
+  watch('app/scripts/src/**/*.js').on('change', browserSync.reload)
+}
 
-});
-
-//compiling our Javascripts
-gulp.task('scripts', function() {
-    //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
-                //prevent pipe breaking caused by errors from gulp plugins
-                .pipe(plumber())
-                //this is the filename of the compressed version of our JS
-                .pipe(concat('app.js'))
-                //catch errors
-                .on('error', gutil.log)
-                //where we will store our finalized, compressed script
-                .pipe(gulp.dest('wp-content/themes/hypetur/scripts'))
-                //notify browserSync to refresh
-                .pipe(browserSync.reload({stream: true}));
-});
-
-//compiling our Javascripts for deployment
-gulp.task('scripts-deploy', function() {
-    //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
-                //prevent pipe breaking caused by errors from gulp plugins
-                .pipe(plumber())
-                //this is the filename of the compressed version of our JS
-                .pipe(concat('app.js'))
-                //compress :D
-                .pipe(uglify())
-                //where we will store our finalized, compressed script
-                .pipe(gulp.dest('wp-content/themes/hypetur/scripts'));
-});
-
-//compiling our SCSS files
-gulp.task('styles', function() {
-    //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
-                //prevent pipe breaking caused by errors from gulp plugins
-                .pipe(plumber({
-                  errorHandler: function (err) {
-                    console.log(err);
-                    this.emit('end');
-                  }
-                }))
-                //get sourceMaps ready
-                .pipe(sourceMaps.init())
-                //include SCSS and list every "include" folder
-                .pipe(sass({
-                      errLogToConsole: true,
-                      includePaths: [
-                          'app/styles/scss/'
-                      ]
-                }))
-                .pipe(autoprefixer({
-                   browsers: autoPrefixBrowserList,
-                   cascade:  true
-                }))
-                //catch errors
-                .on('error', gutil.log)
-                //the final filename of our combined css file
-                .pipe(concat('style.css'))
-                //get our sources via sourceMaps
-                .pipe(sourceMaps.write())
-                //where to save our final, compressed css file
-                .pipe(gulp.dest('wp-content/themes/hypetur'))
-                //notify browserSync to refresh
-                .pipe(browserSync.reload({stream: true}));
-});
-
-//compiling our SCSS files for deployment
-gulp.task('styles-deploy', function() {
-    //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
-                .pipe(plumber())
-                //include SCSS includes folder
-                .pipe(sass({
-                      includePaths: [
-                          'app/styles/scss',
-                      ]
-                }))
-                .pipe(autoprefixer({
-                  browsers: autoPrefixBrowserList,
-                  cascade:  true
-                }))
-                //the final filename of our combined css file
-                .pipe(concat('style.css'))
-                .pipe(minifyCSS())
-                //where to save our final, compressed css file
-                .pipe(gulp.dest('wp-content/themes/hypetur'));
-});
-
-//basically just keeping an eye on all HTML files
-gulp.task('html', function() {
-    //watch any and all HTML files and refresh when something changes
-    return gulp.src('wp-content/themes/hypetur/*.php')
-        .pipe(plumber())
-        .pipe(browserSync.reload({stream: true}))
-        //catch errors
-        .on('error', gutil.log);
-});
-
-//migrating over all HTML files for deployment
-// gulp.task('html-deploy', function() {
-//     //grab everything, which should include htaccess, robots, etc
-//     gulp.src('app/*')
-//         //prevent pipe breaking caused by errors from gulp plugins
-//         .pipe(plumber())
-//         .pipe(gulp.dest('hype-tur'));
-//
-//     //grab any hidden files too
-//     gulp.src('app/.*')
-//         //prevent pipe breaking caused by errors from gulp plugins
-//         .pipe(plumber())
-//         .pipe(gulp.dest('hype-tur'));
-//
-//     gulp.src('app/fonts/**/*')
-//         //prevent pipe breaking caused by errors from gulp plugins
-//         .pipe(plumber())
-//         .pipe(gulp.dest('hype-tur/fonts'));
-//
-//     //grab all of the styles
-//     gulp.src(['app/styles/*.css', '!app/styles/style.css'])
-//         //prevent pipe breaking caused by errors from gulp plugins
-//         .pipe(plumber())
-//         .pipe(gulp.dest('hype-tur'));
-// });
-
-//cleans our dist directory in case things got deleted
-gulp.task('clean', function() {
-    return shell.task([
-      'rm -rf dist'
-    ]);
-});
-
-//create folders using shell
-gulp.task('scaffold', function() {
-  return shell.task([
-      'mkdir wp-content/themes/hypetur',
-      'mkdir wp-content/themes/hypetur/fonts',
-      'mkdir wp-content/themes/hypetur/images',
-      'mkdir wp-content/themes/hypetur/scripts',
-      'mkdir wp-content/themes/hypetur/styles'
-    ]
-  );
-});
-
-//this is our master task when you run `gulp` in CLI / Terminal
-//this is the main watcher to use when in active development
-//  this will:
-//  startup the web server,
-//  start up browserSync
-//  compress all scripts and SCSS files
-gulp.task('default', ['browserSync', 'scripts', 'styles'], function() {
-    //a list of watchers, so it will watch all of the following files waiting for changes
-    gulp.watch('app/scripts/src/**', ['scripts']);
-    gulp.watch('app/styles/scss/**', ['styles']);
-    gulp.watch('app/images/**', ['images']);
-    gulp.watch('app/*.php', ['html']);
-});
-
-//this is our deployment task, it will set everything for deployment-ready files
-gulp.task('deploy', gulpSequence('clean', 'scaffold', ['scripts-deploy', 'styles-deploy', 'images-deploy']));
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+  parallel(css, js),
+  watchTask
+)
